077848eb294e88859d3a081e8a331a0f395b1a1d
Duncan Boyes <boyes.duncan@gmail.com>
2020-05-20 17:27:39 -0600

    Port over from Unity 2018
    Base Scene still does not work.
    Sticks works
    Hairpin works

---
+++ uber_monkey_ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform focus;
+    [Tooltip("Orbit distance from target")]
+    [SerializeField,Range(1f,50f)]
+    public float orbitDistance = 1f;
+    [Tooltip("The radius from focus that which when the camera is outside of, will begin to move the camera to focus")]
+    [SerializeField, Range(0.1f, 4f)]
+    public float focusRadius = 1f;
+    [Tooltip("The strength of the focus centering effect. Centers the focus gradually on the target")]
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+    private Vector3 velocity;
+    [Tooltip("Speed of Camera Rotations, manual and automatic")]
+    [SerializeField, Range(5f, 100f)]
+    public float cameraRotateSpeed = 40f;
+    float camRotate;
+
+
+    // Start is called before the first frame update
+    Vector3 focusPoint;
+    public Vector2 orbitAngles = new Vector2(30f,0);
+    void Start()
+    {
+        focusPoint = focus.position;
+    }
+
+    void ManualRotation()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("CameraPitch"), Input.GetAxis("CameraYaw"));
+        float e = 0.15f;
+        if (input.magnitude > e)
+        {
+            Debug.Log(input.magnitude);
+            orbitAngles += input * cameraRotateSpeed * Time.unscaledDeltaTime;
+        }
+    }
+    void UpdateFocusPoint()
+    {
+        Vector3 targetPoint = focus.position;
+        float distance = Vector3.Distance(targetPoint, focusPoint);
+        if (distance > focusRadius)
+        {
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, focusRadius / distance);
+        }
+        if (distance > 0.05f && focusRadius>0.05f)
+        {
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime));
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        UpdateFocusPoint();
+        ManualRotation();
+        Quaternion lookRotation = Quaternion.Euler(orbitAngles);
+        Vector3 lookDirection = lookRotation*Vector3.forward;
+        Vector3 lookPosition = focusPoint - orbitDistance * lookDirection;
+        transform.SetPositionAndRotation(lookPosition,lookRotation);
+    }
+}
---
+++ uber_monkey_ball/Assets/Scripts/LevelController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    //Interface
+    public Transform player;
+    public float maxTilt;
+    public Transform mainCamera;
+    
+    //Protected
+    Transform levelTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelTransform = transform;
+    }
+
+    // Update is called once per frame
+    Quaternion cachedPlayerRot;
+    Vector3 smoothInput;
+    void FixedUpdate()
+    {
+        cachedPlayerRot = player.rotation;
+        player.rotation = Quaternion.identity;
+        Vector3 input = new Vector3(Input.GetAxis("Vertical"),0, -Input.GetAxis("Horizontal"));
+        smoothInput = input / 4 + smoothInput * 3 / 4;
+        Vector3 inputCamSpace = mainCamera.TransformDirection(smoothInput);
+        Quaternion cameraPitchRot = Quaternion.FromToRotation(mainCamera.up, Vector3.up);
+        inputCamSpace = cameraPitchRot * inputCamSpace;
+        Quaternion offsetRot = Quaternion.AngleAxis(smoothInput.magnitude * maxTilt, inputCamSpace);
+        levelTransform.SetParent(player);
+        player.rotation = Quaternion.Inverse(levelTransform.rotation) * player.rotation;
+        player.rotation = offsetRot * player.rotation;
+        player.DetachChildren();
+        player.rotation = cachedPlayerRot;
+    }
+    private void LateUpdate()
+    {
+       
+    }
+}
