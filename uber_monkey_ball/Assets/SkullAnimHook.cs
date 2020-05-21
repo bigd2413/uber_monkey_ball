02f123c2ae19fe5496505cbe419fe48dce4d70bd
Duncan Boyes <boyes.duncan@gmail.com>
2020-05-20 21:59:20 -0600

    Added Animation to Skull

---
+++ uber_monkey_ball/Assets/SkullAnimHook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullAnimHook : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody rigidbody;
+    string animatorParameter = "Blend";
+    Quaternion lastFrameRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponentInParent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float speed = rigidbody.velocity.magnitude;
+        animator.SetFloat(animatorParameter, speed);
+    }
+}
