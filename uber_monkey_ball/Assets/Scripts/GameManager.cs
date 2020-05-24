5a30c8bda9b1bddfc3e84c763a3549782304cda0
Duncan Boyes <boyes.duncan@gmail.com>
2020-05-24 13:38:04 -0600

    Goal Player Behvious
    player now stops and flys away after getting goal

---
+++ uber_monkey_ball/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private static GameManager instance = null;
+    public Transform player;
+    public Camera mainCamera;
+
+    // Game Instance Singleton
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        // if the singleton is not yet instantiated
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        instance = this;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ManageGoal()
+    {
+
+    }
+}
---
+++ uber_monkey_ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 20f;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            rb.useGravity = false;
+            rb.drag = 1f;
+            rb.angularDrag = 1f;
+            StartCoroutine(PlayerGoalBehaviour()); 
+        }
+    }
+    IEnumerator PlayerGoalBehaviour()
+    {
+        float time = 0;
+        yield return new WaitForSeconds(2);
+        while (time < 10)
+        {
+            time += Time.deltaTime;
+            rb.AddForce(Vector3.up*50);
+            yield return null;
+        }
+    } 
+}
