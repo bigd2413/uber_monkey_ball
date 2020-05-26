b0f7e5546dfcae020e93f52976d126f99d2c664e
TopHat-x <49600243+TopHat-x@users.noreply.github.com>
2020-05-26 12:13:17 -0600

    Collectable pick up sound effects and music added

---
+++ uber_monkey_ball/Assets/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private static GameManager instance = null;
+    public Transform player;
+    public Camera mainCamera;
+
+    // Game Instance Singleton
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        // if the singleton is not yet instantiated
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        instance = this;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ManageGoal()
+    {
+        StartCoroutine(GoalRoutine());
+    }
+    public void ManageFalloff()
+    {
+        StartCoroutine(FalloffRoutine());
+    }
+    IEnumerator GoalRoutine()
+    {
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    IEnumerator FalloffRoutine()
+    {
+        FindObjectOfType<AudioManager>().Play("PlayerDeath");
+        FindObjectOfType<AudioManager>().Play("PlummetSound");
+
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ uber_monkey_ball/Assets/Scripts/BoneCollectable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneCollectable : MonoBehaviour
+{
+    public AudioSource audioSource;
+    Animator BoneAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        BoneAnimator = GetComponent<Animator>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Play GetBone animation
+            StartCoroutine(GetBoneRoutine());
+        }
+    }
+    IEnumerator GetBoneRoutine()
+    {
+        audioSource.Play();
+        BoneAnimator.SetTrigger("GetBone");
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
