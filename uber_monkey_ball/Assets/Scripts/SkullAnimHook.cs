950ace31d0cf72a89821c3b868e4aeb43b6ce4f9
Duncan Boyes <boyes.duncan@gmail.com>
2020-05-26 14:59:51 -0600

    Player Now stabilizes at low enough speeds. No more storage.ide

--- uber_monkey_ball/Assets/Scripts/SkullAnimHook.cs
+++ uber_monkey_ball/Assets/Scripts/SkullAnimHook.cs
@@ -7,7 +7,9 @@ public class SkullAnimHook : MonoBehaviour
     Animator animator;
     Rigidbody rb;
     string animatorParameter = "Blend";
-    Quaternion lastFrameRot;
+    Vector3 smoothedVelocity;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,29 @@ public class SkullAnimHook : MonoBehaviour
         rb = GetComponentInParent<Rigidbody>();
     }
 
+    private void Update()
+    {
+    }
+
     // Update is called once per frame
     void LateUpdate()
     {
+        //Correct to stay upright and keep animations
+        Quaternion animRotOffset = transform.localRotation;
+        smoothedVelocity = 0.1f * rb.velocity + 0.9f * smoothedVelocity;
+        if (smoothedVelocity.magnitude > 0.0001f)
+        {
+            float maxVelWithControl = 6;
+            // From 0 to maxVel => full control, maxVel to 4*maxVel => linear descent to 0. Output Clamp 0 to 1
+            float correctionMagnitude = Mathf.Clamp01(-0.5f *Mathf.Max(smoothedVelocity.magnitude,maxVelWithControl) + maxVelWithControl);
+            // Slerp between no rotation and full corrective rotation
+            Quaternion correctionRot = Quaternion.Slerp(Quaternion.identity,Quaternion.FromToRotation(transform.forward,smoothedVelocity),correctionMagnitude);
+            Debug.Log(correctionMagnitude + "/ " + smoothedVelocity.magnitude);
+            transform.forward = correctionRot*transform.forward;
+            transform.forward = animRotOffset * transform.forward;
+        }
+
+        // Blend anims with speed
         float speed = rb.velocity.magnitude;
         animator.SetFloat(animatorParameter, speed);
     }
