7f9adb59e0ca73dd3c7276406bb340cd343740b8
TopHat-x <49600243+TopHat-x@users.noreply.github.com>
2020-06-03 14:34:31 -0600

    Fixed multi-goal bug

--- uber_monkey_ball/Assets/Scripts/PlayerController.cs
+++ uber_monkey_ball/Assets/Scripts/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Goal"))
+        if (other.CompareTag("Goal") && !goalMet)
         {
             rb.useGravity = false;
             rb.drag = 1f;
